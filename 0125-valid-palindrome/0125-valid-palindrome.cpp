a010d2a5cd00fc197d7bd7e22dc0518d7065256b
Aastha <136194488+aastha-0711@users.noreply.github.com>
2025-04-01 17:14:58 +0530

    Time: 2 ms (39.86%), Space: 10.3 MB (27.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s2;
-        using std::isalnum;
-        using std::tolower;
-        for(char c:s){
-            if(isalnum(c)){
-                s2.push_back(tolower(c));
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
             }
         }
-        int left=0;
-        int right=s2.size()-1;
-        while(left<right){
-if(s2[right]!=s2[left]){
-    return false;
-}left++;
-        right--;}
-        return true;
+
+        int left = 0;
+        int right = filtered.size() - 1;
+
+        while (left < right) {
+            if (filtered[left] != filtered[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;        
     }
 };
