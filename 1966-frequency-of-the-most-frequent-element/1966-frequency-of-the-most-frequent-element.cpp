d5235c86ff0795edc8f493a040c448a2a2de1f57
Aastha <aasthagarg0711@gmail.com>
2025-06-03 22:31:37 +0530

    Time: 35 ms (76.53%), Space: 105.8 MB (90.59%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -6,7 +6,7 @@ public:
         long long maxFreq=0,l=0,r=0,total=0, n=nums.size();
         for(r=0;r<n;r++){
             total+=nums[r];
-            while(l+1<=r && (nums[r]*(r-l+1)>total+k)){
+            while(l<r && (nums[r]*(r-l+1)>total+k)){
                 total-=nums[l];
                 l++;
             }
