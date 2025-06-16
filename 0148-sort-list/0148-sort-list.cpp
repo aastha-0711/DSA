98c3f47fe1992acaff24549893d4c866a40d4e26
Aastha <aasthagarg0711@gmail.com>
2025-06-16 23:31:42 +0530

    Time: 45 ms (45.19%), Space: 75.6 MB (48.02%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,24 +9,59 @@
  * };
  */
 class Solution {
+private:
+ // Returns the node *before* the middle so we can break the list
+ListNode* findmiddle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    ListNode* prev = nullptr;
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return prev;
+}
+
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        if (list1) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
+        }
+        return dummyNode->next;
+    }
+
+ListNode* mergesort(ListNode* head) {
+    if (!head || !head->next) return head;
+
+    ListNode* midPrev = findmiddle(head);
+    ListNode* mid = midPrev->next;
+    midPrev->next = nullptr;  // Break the list into two halves
+
+    ListNode* left = mergesort(head);
+    ListNode* right = mergesort(mid);
+    return merge(left, right);
+}
+
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int> ans;
-        if(head==nullptr || head->next==nullptr){
+        if (head == nullptr || head->next == nullptr) {
             return head;
         }
-        ListNode* current=head;
-        while(current!=nullptr){
-            ans.push_back(current->val);
-            current=current->next;
-        }
-        sort(ans.begin(),ans.end());
-        current=head;
-        for(int i=0;i<ans.size();i++){
-current->val=ans[i];
-current=current->next;
-        }
-        return head;
-
+        return mergesort(head);
     }
 };
