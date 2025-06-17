dea62f62be8bed2d02245dd6bb2c7daab4bc0831
Aastha <aasthagarg0711@gmail.com>
2025-06-17 17:17:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-double myPow(double x, int n) {
-  double ans = 1.0;
-  long long nn = n;
-  if (nn < 0) nn = -1 * nn;
-  while (nn) {
-    if (nn % 2) {
-      ans = ans * x;
-      nn = nn - 1;
-    } else {
-      x = x * x;
-      nn = nn / 2;
+    double myPow(double x, int n) {
+        long long nn = n; // Safely promote to avoid overflow
+        if (nn < 0) {
+            x = 1 / x;
+            nn = -nn;
+        }
+
+        if (nn == 0) return 1;
+        if (nn % 2 == 0) {
+            return myPow(x * x, nn / 2);
+        } else {
+            return x * myPow(x, nn - 1);
+        }
     }
-  }
-  if (n < 0) ans = (double)(1.0) / (double)(ans);
-  return ans;
-}};
+};
