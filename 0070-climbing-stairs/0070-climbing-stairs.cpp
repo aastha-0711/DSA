b24ddcbb8e45f86032350d738bbaaca1b85e8af5
Aastha <aasthagarg0711@gmail.com>
2025-07-10 11:55:54 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-       int a=1, b=2, c;
-        for(int i=3;i<=n;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-            
+        if (n <= 1) return 1;
+
+        int prev1 = 1; // ways to reach step 1
+        int prev2 = 1; // ways to reach step 0
+        int curr = 0;
+
+        for (int i = 2; i <= n; i++) { 
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return c;
-    }};
+        return curr;
+    }
+};
