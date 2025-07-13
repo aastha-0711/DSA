5b0d4f93251b2a5253c43f588063d4e0db4d222b
Aastha <aasthagarg0711@gmail.com>
2025-07-13 14:50:20 +0530

    Time: 19 ms (13.58%) | Memory: 17 MB (25.62%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -8,7 +8,7 @@ public:
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (word1[i - 1] == word2[j - 1]) {
+                if (word1[i - 1] == word2[j - 1]){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     maxLen = max(maxLen, dp[i][j]);
                 } else {
