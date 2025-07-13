1dc2d7701db5f1f75fa0796f9e9d7984fe96ad53
Aastha <aasthagarg0711@gmail.com>
2025-07-13 08:41:26 +0530

    Time: 26 ms (38.12%) | Memory: 46.5 MB (6.72%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -7,7 +7,7 @@ public:
             t[i][0]=1;
         }
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=amount;j++){
+            for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
                     t[i][j]=t[i][j-coins[i-1]]+t[i-1][j];
                 }
