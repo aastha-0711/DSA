94c310f10370c2468a2d8ebb5c8908d97baba544
Aastha <aasthagarg0711@gmail.com>
2025-07-24 22:51:42 +0530

    Time: 3 ms (88.48%), Space: 71.5 MB (68.99%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int res = 0, count = 0;
-        for (int l = 0, r = 0; r < nums.size(); r++){
-            if (nums[r] % 2){
-                k--;
-                count = 0;
-            }
-            while (k == 0){
-                count++;
-                k += (nums[l++] % 2);
-            }
-            res += count;
-        }
-        return res;
+        int res = 0, count = 0, left = 0;
+for (int r = 0; r < nums.size(); r++) {
+    if (nums[r] % 2) {
+        k--;
+        count=0; // each odd reduces k
+    }
+    while (k == 0) {  // valid window with exactly k odds
+        count++;              // count valid subarrays ending at r
+        k += (nums[left++] % 2);  // shrink window from left
+    }
+
+    res += count;  // add all valid subarrays ending at r
+}
+return res;
     }
 };
