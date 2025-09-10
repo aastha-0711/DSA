9e1aa1433ce8b346fd3d8a505308c011c167f342
Aastha <aasthagarg0711@gmail.com>
2025-09-10 16:04:19 +0530

    Time: 0 ms (100%), Space: 14.4 MB (62.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,36 +4,52 @@ public:
         int m = board.size();
         if (m == 0) return;
         int n = board[0].size();
-        vector<pair<int,int>> directions = {{0,1},{0,-1},{1,0},{-1,0}};
 
-        auto markSafe = [&](int i, int j) {
-            stack<pair<int,int>> st;
-            st.push({i,j});
-            while (!st.empty()) {
-                auto [x, y] = st.top(); st.pop();
-                if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') continue;
-                board[x][y] = 'T'; // Mark temporarily safe
-                for (auto [dx, dy] : directions) {
-                    st.push({x + dx, y + dy});
+        queue<pair<int,int>> q;
+        vector<pair<int,int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        // Step 1: Push all border 'O' into the queue
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                q.push({i,0});
+                board[i][0] = 'T'; // mark visited safe
+            }
+            if (board[i][n-1] == 'O') {
+                q.push({i,n-1});
+                board[i][n-1] = 'T';
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                q.push({0,j});
+                board[0][j] = 'T';
+            }
+            if (board[m-1][j] == 'O') {
+                q.push({m-1,j});
+                board[m-1][j] = 'T';
+            }
+        }
+
+        // Step 2: BFS flood-fill to mark all border-connected 'O' as 'T'
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto [dx, dy] : directions) {
+                int xx = x + dx, yy = y + dy;
+                if (xx >= 0 && xx < m && yy >= 0 && yy < n && board[xx][yy] == 'O') {
+                    q.push({xx, yy});
+                    board[xx][yy] = 'T';
                 }
             }
-        };
-
-        // Step 1: Mark border-connected 'O's
-        for (int i = 0; i < m; ++i) {
-            if (board[i][0] == 'O') markSafe(i, 0);
-            if (board[i][n - 1] == 'O') markSafe(i, n - 1);
-        }
-        for (int j = 0; j < n; ++j) {
-            if (board[0][j] == 'O') markSafe(0, j);
-            if (board[m - 1][j] == 'O') markSafe(m - 1, j);
         }
 
-        // Step 2: Flip all remaining 'O' to 'X', and 'T' back to 'O'
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        // Step 3: Flip remaining 'O' -> 'X', and 'T' -> 'O'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == 'O') board[i][j] = 'X';
-                if (board[i][j] == 'T') board[i][j] = 'O';
+                else if (board[i][j] == 'T') board[i][j] = 'O';
             }
         }
     }
