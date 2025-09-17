e94220a0cf7657b8fb08ba2f17abd0023080461f
Aastha <aasthagarg0711@gmail.com>
2025-09-17 13:55:50 +0530

    Time: 7 ms (69.1%), Space: 15.2 MB (58.64%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
+          int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        // Edge case: no valid partition
         if ((target + sum) % 2 != 0 || abs(target) > sum)
             return 0;
-
-        int a = (target + sum) / 2;
-
-        vector<vector<int>> dp(n + 1, vector<int>(a + 1, 0));
-        dp[0][0] = 1; // base case: 1 way to get sum 0 using 0 elements
-
-        for (int i = 1; i <= n; i++) {
+             int a = (target + sum) / 2;
+             vector<vector<int>> dp(n, vector<int>(a + 1, 0));
+             dp[0][0] = 1;
+             if (nums[0] <= a) dp[0][nums[0]] += 1;
+              for (int i = 1; i < n; i++) {
             for (int j = 0; j <= a; j++) {
-                // Don't pick current number
-                dp[i][j] = dp[i - 1][j];
-
-                // Pick current number (nums[i-1]) if j is big enough
-                if (j - nums[i - 1] >= 0)
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                dp[i][j] += dp[i - 1][j];  // not take current
+                if (j - nums[i] >= 0)
+                    dp[i][j] += dp[i - 1][j - nums[i]];  // take current
             }
         }
 
-        return dp[n][a];
+        return dp[n - 1][a];
     }
 };
