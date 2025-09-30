ac64ad3294e75f0016343c6ecd1dbab1cc9f7cba
Aastha <aasthagarg0711@gmail.com>
2025-09-30 16:28:06 +0530

    Time: 236 ms (7.29%), Space: 17.5 MB (55.8%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,30 +3,19 @@ public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
-
-        // If total sum is odd, we cannot divide it into 2 equal subsets
         if (totalSum % 2 != 0) return false;
-
-        int target = totalSum / 2;
-
-        // dp[i][j] = true if we can form sum j using first i elements
+        int target = totalSum/ 2;
         vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-        // Sum 0 is always possible (by taking nothing)
         for (int i = 0; i <= n; i++) {
             dp[i][0] = true;
         }
-
-        // Build the table
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
                 if (nums[i - 1] <= j) {
-                    // Take or don't take the current number
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    // Cannot take the number
-                    dp[i][j] = dp[i - 1][j];
-                }
+                    dp[i][j] = dp[i][j] + dp[i - 1][j - nums[i - 1]];
+                } 
+                 
             }
         }
 
